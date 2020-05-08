8222e7732642f44751e8390fbbd2eeb575eed7bc
Aminul Islam Alvi <aminulialvi@gmail.com>
2020-05-08 15:49:39 +0600

    Added: Homepage and Sidebar

---
+++ android/app/src/main/kotlin/com/triamatter/humanfirst/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.triamatter.humanfirst
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
