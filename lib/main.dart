c1d1d25a1fde7c534aa862041f075f56e90d3d1a
Aminul Islam Alvi <aminulialvi@gmail.com>
2020-05-09 22:23:41 +0600

    Added: SignUp and LogIn UI in Authenticate Directory

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:humanfirst/screens/checklist_screen.dart';
 import 'package:humanfirst/screens/fooditem_screen.dart';
-import 'package:humanfirst/screens/home_screen.dart';
 import 'package:humanfirst/screens/helpcenters_screen.dart';
 import 'package:humanfirst/screens/askforhelp_screen.dart';
 import 'package:humanfirst/screens/settings_screen.dart';
 import 'package:humanfirst/screens/statistics_screen.dart';
+import 'package:humanfirst/authenticate/login_screen.dart';
 
 void main() => runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: HomeScreen(),
+    home: LogInPage(),
     routes: {
       '/checklist' : (context) => ChecklistScreen(),
       '/fooditem' : (context) => FoodItemScreen(),
@@ -17,6 +17,8 @@ void main() => runApp(MaterialApp(
       '/askforhelpitem' : (context) => AskForHelpScreen(),
       '/statisticsitem' : (context) => StatisticsScreen(),
       '/settingsitem' : (context) => SettingsScreen(),
+
+
 
     },
 ));
